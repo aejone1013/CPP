90cadb57cd597fbf1966fc2ae6bd8b8e0d9a1112
root <jaoh@student.42.fr>
2025-09-25 10:10:27 +0200

    updated

---
+++ cpp04/ex02/Brain.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaoh <jaoh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/23 14:49:35 by jaoh              #+#    #+#             */
+/*   Updated: 2025/09/25 10:08:46 by jaoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BRAIN_HPP
+# define BRAIN_HPP
+
+#include <iostream>
+#include <string>
+
+class Brain {
+private:
+    std::string ideas[100];  // 100개의 아이디어 배열
+
+public:
+    // Orthodox Canonical Form
+    Brain();
+    Brain(const Brain& other);
+    Brain& operator=(const Brain& other);
+    ~Brain();
+
+    // 유틸리티 함수들
+    void setIdea(int index, const std::string& idea);
+    std::string getIdea(int index) const;
+    void printIdeas() const;
+};
+
+#endif
